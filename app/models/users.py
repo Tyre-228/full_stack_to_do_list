4bec50096bb95a63b30caf4161a60c5008893a2d
Tyre-228 <kiril228panko@gmail.com>
2024-07-19 10:26:00 +0100

    users class has been created and finished v0.2.0

---
+++ app/models/users.py
@@ -0,0 +1,63 @@
+import psycopg2
+import sys
+import os
+
+
+# Adding the database directory so that I can import load_config function from config module
+current_dir = os.path.dirname(os.path.abspath(__file__))
+external_modules_path = os.path.join(current_dir, '../database')
+if external_modules_path not in sys.path:
+    sys.path.insert(0, external_modules_path)
+
+
+from config import load_config
+
+
+class Users:
+    def __init__(self):
+        self.config = load_config()
+    
+    def connect(config):
+        """ Connect to the PostgreSQL database server """
+        try:
+            # connecting to the PostgreSQL server
+            with psycopg2.connect(**config) as conn:
+                print('Connected to the PostgreSQL server.')
+                return conn
+        except (psycopg2.DatabaseError, Exception) as error:
+            print(error)
+    
+    def getUsers(self):
+        with psycopg2.connect(**self.config) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT * FROM users ORDER BY id;")
+                record = cursor.fetchall()
+
+                return record
+    
+    def getById(self, id):
+        with psycopg2.connect(**self.config) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f"SELECT * FROM users WHERE id={str(id)};")
+                record = cursor.fetchone()
+
+                return record
+    
+    def createUser(self, name, email, password):
+        with psycopg2.connect(**self.config) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f"INSERT INTO users(name, email, password) VALUES('{name}', '{email}', '{password}');")
+                cursor.commit()
+    
+    def getId(self, email, password):
+        with psycopg2.connect(**self.config) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f"SELECT id FROM users WHERE email='{email}' AND password='{password}';")
+
+                record = cursor.fetchone()
+
+                # if the record exists, convert it from tuple to string
+                if record: 
+                    record = record[0] 
+
+                return record
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# from app.models.users import Users
+from app.models.users import Users
 # from app.models.tasks import Tasks
 
-# users = Users()
+users = Users()
 # tasks = Tasks()
 
 
-# if __name__ == '__main__':
-#     print(tasks.getByUserId(2))
+if __name__ == '__main__':
+    print(users.createUser("Tyre229", "kirilpanko32@gmail.com", "d2a57e58b8423760f841df6c0ff92aef99a0eac5a6c4d07b573504b0b03ed7a6"))
